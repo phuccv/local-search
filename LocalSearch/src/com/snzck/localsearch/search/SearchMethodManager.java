b73f9aa77f24984282d74244e3db9efa3b0499c4
phuc <phuccv3@gmail.com>
2015-08-21 10:05:03 +0700

    Search mix implement complete but without debug

--- LocalSearch/src/com/snzck/localsearch/search/SearchMethodManager.java
+++ LocalSearch/src/com/snzck/localsearch/search/SearchMethodManager.java
@@ -5,6 +5,7 @@ import localsearch.model.ConstraintSystem;
 import com.snzck.localsearch.SearchMethod;
 import com.snzck.localsearch.SearchTraceable;
 import com.snzck.localsearch.search.method.SearchTabuAssign;
+import com.snzck.localsearch.search.method.SearchTabuMixAssignSwap;
 import com.snzck.localsearch.search.method.SearchTabuSwap;
 import com.snzck.localsearch.system.Config;
 
@@ -24,7 +25,10 @@ public class SearchMethodManager {
 			break;
 		case TABU_SWAP_METHOD:
 			method = new SearchTabuSwap(cs, tracer, config);
-			
+			break;
+		case TABU_MIX_METHOD:
+			method = new SearchTabuMixAssignSwap(cs, tracer, config);
+			break;
 		default:
 			break;
 		}
--- LocalSearch/src/com/snzck/localsearch/search/SearchMethodType.java
+++ LocalSearch/src/com/snzck/localsearch/search/SearchMethodType.java
@@ -3,6 +3,7 @@ package com.snzck.localsearch.search;
 public enum SearchMethodType {
 	TABU_ASSIGN_METHOD,
 	TABU_SWAP_METHOD,
+	TABU_MIX_METHOD,
 	RANDOM_SEARCH,
 	SA_SEARCH
 }
